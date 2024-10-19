736e6eda07519453390d7a8a39480d7f1f884861
Matthew <adetoyesesamson@gmail.com>
2024-10-19 23:51:03 +0100

    Ontap

--- lib/main.dart
+++ lib/main.dart
@@ -35,120 +35,88 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
-      // LISTVIEW NORMAL
-      // body: ListView(
-      //   scrollDirection: Axis.vertical,
-      //   children:  <Widget>[
-
-      //     Container(height: 250, color: Colors.red,),
-      //      Container(
-      //         height: 250,
-      //         color: Colors.black,
-      //       ),
-      //         Container(
-      //         height: 250,
-      //         color: Colors.yellow,
-      //       ),
-      //         Container(
-      //         height: 250,
-      //         color: Colors.green,
-      //       ),
-      //         Container(
-      //         height: 250,
-      //         color: Colors.amber,
-      //       ),
-
-      //         Container(
-      //         height: 250,
-      //         color: Colors.brown,
-      //       ),
-      //   ],
-      // )
-// LISTVIEW.BUILDER
-      body: ListView.builder(
-          itemCount: 10,
-          itemBuilder: (BuildContext context, int index) {
-            return UserTile(name: users[index].name, profession: users[index].profession, image: users[index].image ,);
-            // return UserTile(
-            //   image: users[index].image,
-            //     name: users[index].name, profession: users[index].profession);
-          }),
-
-      // Combo of the the Two ListViw
-      // ListView. Seperated
-      // body: ListView.separated(
-      //     itemBuilder: (BuildContext context, int index) {
-      //       return newWidget(index);
-      //     },
-      //     separatorBuilder: (BuildContext context, int index) {
-      //       return (index + 2) % 3 == 0
-      //           ? ListTile(
-      //               leading: const CircleAvatar(
-      //                 backgroundColor: Colors.orange,
-      //                 child: Text(
-      //                   'A',
-      //                   style: TextStyle(
-      //                       fontSize: 20,
-      //                       fontWeight: FontWeight.bold,
-      //                       color: Colors.white),
-      //                 ),
-      //               ),
-      //               title: const Text('Advertiser'),
-      //               subtitle: const Text('Some information about add'),
-      //               trailing: Container(
-      //                 color: Colors.orange,
-      //                 alignment: Alignment.center,
-      //                 width: 30,
-      //                 height: 20,
-      //                 child: const Text(
-      //                   'Ad',
-      //                   style: TextStyle(
-      //                     fontSize: 12,
-      //                     color: Colors.white,
-      //                   ),
-      //                 ),
-      //               ),
-      //             )
-      //           : const SizedBox(width: 0, height: 0,);
-      //     },
-      //     itemCount: users.length)
-      // body: ListView.custom(
-      //     childrenDelegate: SliverChildListDelegate([
-      // newWidget('Adetoyese', 0, Colors.red)
-
-      // ])),
-    );
+        appBar: AppBar(
+          backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+          title: Text(widget.title),
+        ),
+        // body: Scrollbar(
+        //   thickness: 30,
+        //   interactive: true,
+        //   child: ListView.custom(
+        //       childrenDelegate:
+        //           SliverChildBuilderDelegate((BuildContext context, int index) {
+        //     return UserDelegateTile(
+        //         name: users[index].name,
+        //         image: users[index].image,
+        //         profession: users[index].profession);
+        //   }, childCount: users.length)),
+        // ),
+        body: Column(
+          children: [
+            Container(
+              // color: Colors.black,
+              height: 50,
+              margin: const EdgeInsets.symmetric(vertical: 16),
+              child: ListView.builder(
+                scrollDirection: Axis.horizontal,
+                itemCount: colors.length,
+                itemBuilder: (BuildContext context, int index) {
+                  return InkWell(
+                    onTap: () {
+                      scrollToTap(index);
+                    },
+                    child: Container(
+                      alignment: Alignment.center,
+                      margin: const EdgeInsets.only(right: 16),
+                      height: 50,
+                      width: 50,
+                      color: colors[index],
+                      child: Text(
+                        '${index + 1}',
+                        style: const TextStyle(fontSize: 22),
+                      ),
+                    ),
+                  );
+                },
+              ),
+            ),
+            Expanded(
+              child: ListView.builder(
+                itemCount: colors.length,
+                itemBuilder: (BuildContext context, int index) {
+                  return Container(
+                    height: containerHeight,
+                    color: colors[index],
+                  );
+                },
+              ),
+            )
+          ],
+        ));
   }
 }
 
-// Widget newWidget({String? name, int? index, Color? color}) {
-//   return Container(
-//     height: 245,
-//     margin: const EdgeInsets.all(8),
-//     alignment: Alignment.center,
-//     color: color,
-//     child: Text('$name No $index'),
-//   );
+// class UserDelegateTile extends StatelessWidget {
+//   const UserDelegateTile(
+//       {super.key,
+//       required this.name,
+//       required this.image,
+//       required this.profession});
+//   final String? name;
+//   final String? image;
+//   final String? profession;
+//   @override
+//   Widget build(BuildContext context) {
+//     return  ListTile(
+//         leading: CircleAvatar(
+//           // radius: 70,
+//         backgroundColor: Colors.teal,
+//           backgroundImage: AssetImage('$image'),
+//         ),
+//         title: Text('$name'),
+//         subtitle: Text('$profession'),
+//         trailing: Icon(Icons.arrow_forward_ios),
+//       );
+    
+//   }
 // }
-class UserTile extends StatelessWidget {
-  const UserTile({super.key, this.name, this.profession, this.image});
-  final String? name;
-  final String? profession;
-  final String? image;
-  @override
-  Widget build(BuildContext context) {
-    return ListTile(
-      leading: CircleAvatar(
-        radius: 10,
-        backgroundImage: AssetImage('$image'),
-      ),
-      title: Text('$name'),
-      subtitle: Text('$profession'),
-      trailing: Icon(Icons.arrow_forward_ios),
-    );
-  }
-}
--- lib/user.dart
+++ lib/user.dart
@@ -1,26 +1,45 @@
-class User {
-  final String name;
-  final String image;
-  final String profession;
+import 'package:flutter/material.dart';
+
+// class User {
+//   final String name;
+//   final String image;
+//   final String profession;
 
   // User(this.name, this.image, this.profession);
-  User(this.name, this.profession, this.image);
-}
+//   User(this.name, this.profession, this.image);
+// }
 
-List users = [
- User('Ade', 'Web Dev', 'images/laptop.jpg'),
- User('Sam', 'App Dev', 'images/laptop.jpg'),
- User('Joy', 'Photographer', 'images/laptop.jpg'),
- User('Pece', 'Engr', 'images/laptop.jpg'),
- User('Hope', 'Tailor', 'images/laptop.jpg'),
- User('Favour', 'Constructor', 'images/laptop.jpg'),
- User('Blessing', 'Fire Fighter', 'images/.jpg'),
- User('Evelyn', 'Footballer', 'images/laptop.jpg'),
- User('Grace', 'MAnager', 'images/laptop.jpg'),
- User('Ola', 'Data Anayst', 'images/laptop.jpg'),
-User('Shams', 'Driver', 'images/laptop.jpg'),
-User('Bash', 'Nurse', 'images/laptop.jpg'),
-User('Austin', 'Painter', 'images/laptop.jpg')
+// List users = [
+//   User('Ade', 'Web Dev', 'images/laptop.jpg'),
+//   User('Sam', 'App Dev', 'images/laptop.jpg'),
+//   User('Joy', 'Photographer', 'images/laptop.jpg'),
+//   User('Pece', 'Engr', 'images/laptop.jpg'),
+//   User('Hope', 'Tailor', 'images/laptop.jpg'),
+//   User('Favour', 'Constructor', 'images/laptop.jpg'),
+//   User('Blessing', 'Fire Fighter', 'images/.jpg'),
+//   User('Evelyn', 'Footballer', 'images/laptop.jpg'),
+//   User('Grace', 'MAnager', 'images/laptop.jpg'),
+//   User('Ola', 'Data Anayst', 'images/laptop.jpg'),
+//   User('Shams', 'Driver', 'images/laptop.jpg'),
+//   User('Bash', 'Nurse', 'images/laptop.jpg'),
+//   User('Austin', 'Painter', 'images/laptop.jpg')
+// ];
 
-
+final List<Color> colors = [
+  Colors.teal,
+  Colors.red,
+  Colors.white,
+  Colors.blueGrey,
+  Colors.cyan,
+  Colors.deepPurple,
+  Colors.orange,
+  Colors.yellow,
+  Colors.indigo,
+  Colors.pink,
 ];
+const containerHeight = 300.0;
+final ScrollController scrollController = ScrollController();
+
+void scrollToTap(int index) {
+  scrollController.animateTo(index*containerHeight, duration:const Duration(milliseconds: 500), curve: Curves.easeIn);
+}
